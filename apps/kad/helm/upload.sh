7c7646b5b4fc5ee770761f7a12cb479154ac81cb
Martin Fabík <mar.fabik@gmail.com>
2024-12-10 14:14:22 +0100

    Add: argocd in argocd

--- apps/kad/helm/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-helm package .
-helm repo index . --url https://fi.6shore.net/helm/ --merge index.yaml
-
-
-mc cp index.yaml obj/static/fi.6shore.net/helm/
-mc cp *.tgz obj/static/fi.6shore.net/helm/
-rm -v *.tgz
